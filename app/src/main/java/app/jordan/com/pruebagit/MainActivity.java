d83c6b24667ce2c78d3017392d780ea9484c9e41
Jordan Revata <jordan.revata@tecsup.edu.pe>
2018-03-29 12:09:54 -0500

    Initial commit

---
+++ app/src/main/java/app/jordan/com/pruebagit/MainActivity.java
@@ -0,0 +1,13 @@
+package app.jordan.com.pruebagit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
